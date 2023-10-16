334c96c3d8247dd8fd7c794c512a4ba3f88b5f60
SantiIndra <148076724+SantiIndra@users.noreply.github.com>
2023-10-16 16:25:30 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
+import {MyCoreModule } from '@my/core';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule,
+    AppRoutingModule, MainModule, SecurityModule, MyCoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
