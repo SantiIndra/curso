13d2aa03304be13f6620c3e08cfddd3a254079cf
SantiIndra <148076724+SantiIndra@users.noreply.github.com>
2023-10-19 08:00:05 +0200

    Add files via upload

---
+++ src/lib/independientes/index.ts
@@ -0,0 +1,1 @@
+export * from './grafico-svg/graficos-svg.component'
--- src/lib/my-core/my-core.module.ts
+++ src/lib/my-core/my-core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SizerComponent } from './components/size.component';
+import { PIPES_CADENAS } from './pipes/cadenas.pipes';
+import { PIPES_NUMERICOS } from './pipes/numericos.pipes';
 
 
 
 @NgModule({
-  declarations: [],
-  exports: [],
+  declarations: [ PIPES_CADENAS, PIPES_NUMERICOS, ],
+  exports: [ SizerComponent, PIPES_CADENAS, PIPES_NUMERICOS, ],
   imports: [
-    CommonModule
+    CommonModule, SizerComponent
   ]
 })
 export class MyCoreModule { }
